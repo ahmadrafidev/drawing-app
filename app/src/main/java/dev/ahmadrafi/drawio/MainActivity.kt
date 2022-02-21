0f30be359b0dbd3ced811bf6a4fc06b10b42b7b7
ahmad rafi wirana <ahmad.rafi02@ui.ac.id>
2022-02-21 11:10:40 +0700

    initial commit

---
+++ app/src/main/java/dev/ahmadrafi/drawio/MainActivity.kt
@@ -0,0 +1,16 @@
+package dev.ahmadrafi.drawio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private var drawingView: DrawingView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
+
+    }
+}
